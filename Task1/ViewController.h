55147666b67fbb93afe150f1e40bbf663f83bc09
admin <admin@iMac-admin.local>
2014-12-23 18:05:25 +0600

    Initial Commit

---
+++ Task1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task1
+//
+//  Created by Saibhan on 12/23/14.
+//  Copyright (c) 2014 Saibhan Nadirov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Task1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Task1
+//
+//  Created by Saibhan on 12/23/14.
+//  Copyright (c) 2014 Saibhan Nadirov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
